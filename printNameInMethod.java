f234afd829d0aa0e276d8a4d3b68a26c4aec0d3e
richa laxkar <richalaxkar@monishs-MacBook-Air.local>
2025-03-24 18:03:33 +0530

    printNameInMethod

---
+++ printNameInMethod.java
@@ -0,0 +1,12 @@
+
+import java.util.*;
+
+public class printNameInMethod {
+    static void printNameInMethod(){
+    System.out.println("Richa");
+    }
+    public static void main(String [] args){
+    printNameInMethod();
+    }
+    
+}

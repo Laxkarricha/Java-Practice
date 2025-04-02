0654977f4904702fe70fe400148204dbce87a3bf
richa laxkar <richalaxkar@monishs-MacBook-Air.local>
2025-04-02 15:22:42 +0530

    evenOddNumbers

---
+++ evenOddNumers.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class evenOddNumers {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        if (A %2==0){
+            System.out.println("Yes It is");
+        }
+        else {
+            System.out.println("No it is not");
+        }
+    }
+
+    
+}

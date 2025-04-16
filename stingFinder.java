c2cebdf51cb4704385acea22f79e4a8c47757ccd
richa laxkar <richalaxkar@monishs-MacBook-Air.local>
2025-04-16 23:22:04 +0530

    stingFinder.java

---
+++ stingFinder.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+
+public class stingFinder {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        String A = sc.nextLine();
+        String [] B = A.split(",");
+        if(A.contains("Ta")){
+            System.out.println("Yes");
+        } else{
+            System.out.println("No");
+        }
+
+
+    }
+    
+}

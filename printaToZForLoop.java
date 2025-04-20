16b2295e145807c9f046aeb5d3489439a059b252
richa laxkar <richalaxkar@monishs-MacBook-Air.local>
2025-04-20 15:42:30 +0530

    printaToZForLoop.java

---
+++ printaToZForLoop.java
@@ -0,0 +1,11 @@
+public class printaToZForLoop {
+    public static void main(String [] args){
+        char c = '0';
+        for (c = 'a'; c<='z'; c++){
+            System.out.println(c);
+        }
+
+
+    }
+    
+}

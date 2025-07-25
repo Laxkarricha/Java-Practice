f9520afd2cecac6a7e9a6d5a30dd2e8bb82137e6
richa laxkar <richalaxkar@monishs-MacBook-Air.local>
2025-07-25 17:03:50 +0530

    studentMarks.java

---
+++ studentMarks.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class studentMarks {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner (System.in);
+        int marks= sc.nextInt();
+        if(marks==90){
+            System.out.println("excellent");
+        }
+        else if (marks>=80){
+            System.out.println("Good");
+        }
+        else if (marks>=70){
+            System.out.println("nice");
+        }
+        else if (marks>=60){
+            System.out.println("okay");
+        }
+        else if (marks<60){
+            System.out.println("Good");
+        }
+
+
+    }
+    }
+    
+    

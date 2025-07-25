df127842e39606bd580ae26c2e28de5cb18e2603
richa laxkar <richalaxkar@monishs-MacBook-Air.local>
2025-07-25 17:14:49 +0530

    coomonStringInt.java

---
+++ coomonStringInt.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class coomonStringInt {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int Num = Integer.parseInt(sc.nextLine());
+        String Name = sc.nextLine();
+        System.out.println("Dear " + Name + " You are awsm");
+        for(int i = 0; i<=Num; i++){
+            System.out.println(i);
+        }
+
+    }
+    
+}
